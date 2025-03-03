dad27f064f2b793cfafa3fcb850a32d19bae5b20
sumedh151 <ghavatsumedh@gmail.com>
2025-03-03 16:33:44 -0500

    Page With No Likes.sql

---
+++ Page With No Likes.sql
@@ -0,0 +1,28 @@
+-- https://datalemur.com/questions/sql-page-with-no-likes
+-- using except
+SELECT page_id FROM pages
+EXCEPT
+SELECT page_id FROM page_likes;
+
+
+-- using not in
+SELECT page_id FROM pages
+WHERE page_id NOT IN (SELECT page_id FROM page_likes);
+
+
+-- using left
+SELECT pages.page_id
+FROM pages
+LEFT JOIN page_likes AS likes
+  ON pages.page_id = likes.page_id
+WHERE likes.page_id IS NULL;
+
+
+-- using not exists
+SELECT page_id
+FROM pages
+WHERE NOT EXISTS (
+  SELECT page_id
+  FROM page_likes AS likes
+  WHERE likes.page_id = pages.page_id
+);
